a1c5d15fdd88d91adfd9ec8cfc1fa90e4b88ff4c
Dave Hughes <dave@waveform.org.uk>
2014-04-01 04:51:22 +0100

    Few more bits on the client

--- client.py
+++ client.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+from __future__ import (
+    unicode_literals,
+    absolute_import,
+    print_function,
+    division,
+    )
+str = type('')
+
 import sys
+import re
 import cmdline
 import datetime
 import time
 import select
 import socket
 import SocketServer
+
+from cmdline import COLOR_RED, COLOR_BOLD, COLOR_RESET
 
 class CompoundPiCmd(cmdline.Cmd):
 
@@ -18,28 +29,61 @@ class CompoundPiCmd(cmdline.Cmd):
         self.pprint('Type "help" for more information')
         self.broadcast_address = '192.168.255.255'
         self.broadcast_port = 8000
-        self.client_timeout = 10
+        self.client_timeout = 5
         self.client_count = 2
         self.server_port = 8000
         # Set up a broadcast capable UDP socket
         self.broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    def broadcast(self, data):
-        self.broadcast_socket.sendto(
-            data, (self.broadcast_address, self.broadcast_port))
+    def broadcast(self, data, addresses=None):
+        if isinstance(addresses, str):
+            addresses = [address]
+        elif not addresses:
+            addresses = [self.broadcast_address]
+        for address in addresses:
+            self.broadcast_socket.sendto(data, (address, self.broadcast_port))
 
-    def responses(self):
+    def responses(self, count=None):
+        if not count:
+            count = self.client_count
         result = {}
         start = time.time()
         while time.time() - start < self.client_timeout:
             if select.select([self.broadcast_socket], [], [], 1)[0]:
-                data, address = self.broadcast_socket.recvfrom(128)
+                data, address = self.broadcast_socket.recvfrom(512)
                 assert address not in result
                 result[address] = data
-                if len(result) == self.client_count:
+                if len(result) == count:
                     break
+        if len(result) < count:
+            sys.stdout.write(COLOR_RED + COLOR_BOLD)
+            self.pprint(
+                'Missing response from %d clients' % (
+                    count - len(result)))
+            sys.stdout.write(COLOR_RESET)
         return result
+
+    def do_config(self, arg=''):
+        """
+        Prints the client configuration.
+
+        Syntax: config
+
+        The config command is used to display the current client configuration.
+        Use the related "set" command to alter the configuration.
+
+        cpi> config
+        """
+        self.pprint_table(
+            [('Setting', 'Value')] +
+            [(name, getattr(self, name)) for name in (
+                'broadcast_address',
+                'broadcast_port',
+                'client_count',
+                'client_timeout',
+                )]
+            )
 
     def do_ping(self, arg=''):
         """
@@ -58,9 +102,49 @@ class CompoundPiCmd(cmdline.Cmd):
         responses = self.responses()
         self.pprint_table(
             [('Address', 'Response')] +
-            [('%s:%d' % address, response.strip()) for (address, response) in responses.items()]
+            [('%s' % address[0], response.strip())
+                for (address, response) in responses.items()]
             )
 
+    def do_resolution(self, arg=''):
+        arg = arg.split()
+        width, height, addresses = arg[0], arg[1], arg[2:]
+        width, height = int(width), int(height)
+        self.broadcast('RESOLUTION %s %s\n', addresses)
+        responses = self.responses(len(addresses))
+
+    status_re = re.compile(
+            r'RESOLUTION (?P<width>\d+) (?P<height>\d+)\n'
+            r'FRAMERATE (?P<rate>\d+(.\d+)?)\n'
+            r'TIMESTAMP (?P<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6})\n')
+    def do_status(self, arg=''):
+        """
+        Retrieves status from selected Pi's on the current subnet.
+
+        Syntax: status [address]...
+
+        The status command is used to retrieve configuration information from
+        clients. It sends the 'STATUS' message to the specified addresses or
+        all addresses on the current subnet if no addresses are specified.
+
+        cpi> status
+        """
+        addresses = arg.split()
+        self.broadcast('STATUS\n', addresses)
+        responses = [
+            (address, self.status_re.match(data))
+            for (address, data) in self.responses(len(addresses)).items()
+            ]
+        self.pprint_table(
+            [('Address', 'Resolution', 'Framerate', 'Timestamp')] +
+            [(
+                address[0],
+                '%sx%s' % (match.group('width'), match.group('height')),
+                '%sfps' % match.group('rate'),
+                match.group('time')
+                )
+                for (address, match) in responses
+                ])
 
 if __name__ == '__main__':
     proc = CompoundPiCmd()
